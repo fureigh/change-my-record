ba1405c8dca4e02e4056c5fd81240366ce15947f
Fureigh <info@fureigh.com>
2014-06-03 22:14:12 -0700

    Made initial filename updates in PDF application filler.

--- pdf_application_filler.rb
+++ pdf_application_filler.rb
@@ -4,12 +4,13 @@
 
 require 'pdf_forms'
 
-pdftk = PdfForms.new('/usr/bin/pdftk')
-field_names = pdftk.get_field_names './calfresh_application.pdf'
+#pdftk = PdfForms.new('/usr/bin/pdftk')
+pdftk = PdfForms.new('/usr/local/bin/pdftk')
+field_names = pdftk.get_field_names './medi_cal_application_english.pdf'
 field_name_hash = Hash.new
 # Create a hash of field names where key == value
 field_names.each do |field_name|
   field_name_hash[field_name] = field_name
 end
 # Fill out each form's field with the field name itself
-pdftk.fill_form './calfresh_application.pdf', "filled_in_form#{Time.now.strftime('%Y%m%d%H%M%S%L')}.pdf", field_name_hash
+pdftk.fill_form './medi_cal_application_english.pdf', "filled_in_form#{Time.now.strftime('%Y%m%d%H%M%S%L')}.pdf", field_name_hash
